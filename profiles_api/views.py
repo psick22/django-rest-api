caabcd60bf4200f391ee65151e19debe43d83d28
psick22 <kgirok@gmail.com>
2021-07-28 15:18:13 +0900

    user profile viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from profiles_api import serializers
 import requests
 from rest_framework import viewsets
 
+from profiles_api.models import UserProfile
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -96,3 +98,9 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = UserProfile.objects.all()
